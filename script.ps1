8d71b779ca31cf75329d30e1aee86e04f13ab358
O-Xian <24855141+O-Xian@users.noreply.github.com>
2023-05-04 13:44:05 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -8,13 +8,16 @@ Mount the volume with VeraCrypt.
 
 You MUST set some variables in the script OR pass them on the command line:
 - $veracryptBinary: full path to VeraCrypt.exe (filename included)
-- $pwdFile: full path (filename included) to a text file containing only your Bitwarden master password
+- $pwdFile: full path (filename included) to a text file containing only your Bitwarden master password (only in non-interactive mode)
 - $itemId: Bitwarden-assigned ID for your volume item
 
-The script expects two environment variables to be set:
+You MUST also provide your credentials, either with environment variables and password file OR use the interactive mode.
+Only in non-interactive mode:
 - BW_CLIENTID: client ID provided by Bitwarden for API access
 - BW_CLIENTSECRET: client secret provided by Bitwarden for API access
 They can be set directly into the script with $env:BW_CLIENTID and $env:BW_CLIENTSECRET or preloaded into your Windows environment. $env:BW_CLIENTSECRET will be unset by the script for security.
+
+In interactive mode, you will be prompted by Bitwarden for your email address and your master password. Interactive mode is activated with the -interactive switch
 
 You MUST first initialize the script by specifying the Bitwarden URL to use with the -server parameter (see example 2 below).
 
@@ -34,8 +37,12 @@ PS> .\vs.ps1 -server https://bitwarden.domain.tld
 PS> .\vs.ps1 -itemId 00000000-1111-2222-3333-444444444444 -veracryptBinary "C:\Program Files\VeraCrypt\VeraCrypt.exe"			 
 
 .EXAMPLE
+PS> .\vs.ps1 -itemId 00000000-1111-2222-3333-444444444444 -interactive			 
+
+.EXAMPLE
 PS> .\vs.ps1 -help
 #>
+[CmdletBinding(DefaultParameterSetName='AuthByKey')]
 param(
 	[Parameter(Mandatory=$false)]
 	[string]
@@ -45,31 +52,44 @@ param(
 	[Parameter(Mandatory=$false)]
 	[string]
 	# Specifies the item ID in Bitwarden vault to fetch
-	$itemId="CHANGE ME TO DEFAULT VALUE",
+	$itemId="CHANGEME",
 	
-	[Parameter(Mandatory=$false)]
+	[Parameter(Mandatory=$false, ParameterSetName="AuthByKey")]
 	[string]
 	# Specifies the full path to Bitwarden master password file
-	$pwdFile="CHANGE ME TO DEFAULT VALUE",
+	$pwdFile="CHANGEME",
+	
+	[Parameter(Mandatory=$false, ParameterSetName="AuthByPassword")]
+	[switch]
+	# Activates interactive mode and prompts for email address and master password instead of using API key and password file.
+	$interactive,
 	
 	[Parameter(Mandatory=$false)]
 	[string]
 	# Specifies the full path to VeraCrypt.exe binary
-	$veracryptBinary="CHANGE ME TO DEFAULT VALUE",
+	$veracryptBinary="C:\Program Files\VeraCrypt\VeraCrypt.exe", #CHANGEME
 	
 	# Displays help message.
 	[switch]$help
 )
 
 if ($help) {
-	echo '.\vc.ps1 [-server <server URL>] [-itemId <id>] [-pwdFile <path\to\pwd\file>] [-veracryptBinary <path\to\veracrypt.exe>]'
-	echo '    Mount the volume'
+	echo 'PS> vc.ps1 [-server <String>] [-itemId <String>] [-pwdFile <String>] [-veracryptBinary <String>] [-help] [<CommonParameters>]'
+	echo '    Mount the volume in non-interactive mode'
+	echo ''
+	echo 'PS> vc.ps1 [-server <String>] [-itemId <String>] [-interactive] [-veracryptBinary <String>] [-help] [<CommonParameters>]'
+	echo '    Mount the volume in interactive mode and prompt for email/master password'
+	echo ''
+	echo 'For more help, see'
+	echo 'PS> help C:\full\path\to\script.ps1'
+	
 	Exit 0
 }
 
-
-$env:BW_CLIENTID="user.CHANGEME"
-$env:BW_CLIENTSECRET="CHANGEME"
+if (-Not $interactive) {
+	$env:BW_CLIENTID="user.CHANGME"
+	$env:BW_CLIENTSECRET="CHANGEME"
+}
 
 #########
 # Is the user trying to change the Bitwarden server?
@@ -87,20 +107,30 @@ if ($server) {
 # password on one side and the whole item on the other
 #########
 Write-Progress -Id 2 -Activity 'Working with Bitwarden' -CurrentOperation 'Login' -Status 'Progress:' -PercentComplete 0
-bw.exe login --apikey *> $null
-Write-Progress -Id 2 -Activity 'Working with Bitwarden' -CurrentOperation 'Unlock the vault' -Status 'Progress:' -PercentComplete 13
-$sessionKey=bw.exe unlock --raw --passwordfile $pwdFile
+
+if ($interactive) {
+	bw.exe login
+} else {
+	bw.exe login --apikey *> $null
+	Write-Progress -Id 2 -Activity 'Working with Bitwarden' -CurrentOperation 'Unlock the vault' -Status 'Progress:' -PercentComplete 13
+	$sessionKey=bw.exe unlock --raw --passwordfile $pwdFile
+}
 
 Write-Progress -Id 2 -Activity 'Working with Bitwarden' -CurrentOperation 'Retrieve the item' -Status 'Progress:' -PercentComplete 25
 $pass=bw.exe get password $itemId --session $sessionKey
+
 Write-Progress -Id 2 -Activity 'Working with Bitwarden' -CurrentOperation 'Retrieve the item' -Status 'Progress:' -PercentComplete 38
 $item=bw.exe get item $itemId --raw --session $sessionKey
 
 Write-Progress -Id 2 -Activity 'Working with Bitwarden' -CurrentOperation 'Lock the vault' -Status 'Progress:' -PercentComplete 50
 bw.exe lock *> $null
+
 Write-Progress -Id 2 -Activity 'Working with Bitwarden' -CurrentOperation 'Logout' -Status 'Progress:' -PercentComplete 63
 bw.exe logout *> $null
-$env:BW_CLIENTSECRET=$null
+
+if (-Not $interactive) {
+	$env:BW_CLIENTSECRET=$null
+}
 
 #########
 # Process the values
